c6726f02e31fc52daf4d838fb228d78f85c7c159
Vendula Svastalova <vendula.svastalova@jetbrains.com>
2025-04-28 12:00:03 +0200

    Revert "Change defaultLocale to autoupdatingCurrent instead of first preferred language"
    This reverts commit b8313183306bdbeabeb56e30a9fc25a04cf0c72d.

--- compose/material3/material3/src/darwinMain/kotlin/androidx/compose/material3/CalendarLocale.darwin.kt
+++ compose/material3/material3/src/darwinMain/kotlin/androidx/compose/material3/CalendarLocale.darwin.kt
@@ -19,7 +19,6 @@ package androidx.compose.material3
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.ReadOnlyComposable
 import platform.Foundation.NSLocale
-import platform.Foundation.autoupdatingCurrentLocale
 import platform.Foundation.preferredLanguages
 
 
@@ -27,4 +26,6 @@ actual typealias CalendarLocale = NSLocale
 
 @Composable
 @ReadOnlyComposable
-internal actual fun defaultLocale(): CalendarLocale = NSLocale.autoupdatingCurrentLocale()
+internal actual fun defaultLocale(): CalendarLocale = NSLocale(
+    NSLocale.preferredLanguages.first() as String
+)
