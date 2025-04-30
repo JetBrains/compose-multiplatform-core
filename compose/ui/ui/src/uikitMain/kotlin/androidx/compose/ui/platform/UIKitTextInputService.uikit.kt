cfeaad03468b0a0c938066e0e13bf0889a8fcc91
Andrei Salavei <Andrei.Salavei@jetbrains.com>
2025-04-30 12:09:09 +0200

    Fix keyboard jumps when switching between text fields (#2062)
    Add extra delay to postpone the hide keyboard function call.
    
    Fixes
    https://youtrack.jetbrains.com/issue/CMP-8046/Keyboard-jumps-when-TF-refocuses-programmatically
    
    ## Release Notes
    ### Fixes - iOS
    - _(prerelease fix)_ Fix keyboard jumps when switching between text
    fields

--- compose/ui/ui/src/uikitMain/kotlin/androidx/compose/ui/platform/UIKitTextInputService.uikit.kt
+++ compose/ui/ui/src/uikitMain/kotlin/androidx/compose/ui/platform/UIKitTextInputService.uikit.kt
@@ -58,6 +58,11 @@ import platform.UIKit.UIView
 import platform.UIKit.UIViewAutoresizingFlexibleHeight
 import platform.UIKit.UIViewAutoresizingFlexibleWidth
 
+// Due to unexpected delays between the commands to show/hide the keyboard,
+// it may jump when switching between text fields.
+// Adding a delay to the 'resignFirstResponder' function call to eliminate this issue.
+private val CLEAR_FOCUS_DELAY: Long = 10L
+
 internal class UIKitTextInputService(
     private val updateView: () -> Unit,
     private val view: UIView,
@@ -167,7 +172,7 @@ internal class UIKitTextInputService(
 
     override fun hideSoftwareKeyboard() {
         textUIView?.let {
-            focusStack?.popUntilNext(it)
+            focusStack?.popUntilNext(it, delayMillis = CLEAR_FOCUS_DELAY)
         }
     }
 
@@ -388,6 +393,7 @@ internal class UIKitTextInputService(
 
             view.resetOnKeyboardPressesCallback()
             mainScope.launch {
+                delay(CLEAR_FOCUS_DELAY)
                 view.removeFromSuperview()
             }
         }
