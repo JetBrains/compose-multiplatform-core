749df82ebe0ff767cd3ebc845ff1694f9768d045
Oleksandr.Karpovich <a.n.karpovich@gmail.com>
2025-05-16 09:45:07 +0200

    Revert "change the way :mpp collects test tasks"
    This reverts commit 6dff5868b678741222c15f30eabf5030ec59ddc7.

--- compose/ui/ui/src/desktopTest/kotlin/androidx/compose/ui/awt/ComplexApplicationTest.kt
+++ compose/ui/ui/src/desktopTest/kotlin/androidx/compose/ui/awt/ComplexApplicationTest.kt
@@ -134,7 +134,6 @@ import kotlin.random.Random
 import kotlinx.coroutines.DelicateCoroutinesApi
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.delay
-import org.junit.Ignore
 import org.junit.Test
 
 @Suppress("ConstPropertyName")
@@ -652,7 +651,6 @@ private suspend fun performGC() {
 
 private val availableMemory get() = Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory()
 
-@Ignore
 class ComplexApplicationTest {
     @Test
     fun `no memory leak when open window multiple times`() = runApplicationTest(
