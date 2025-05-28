d2fd2490929b4df8a920f2db94170b09e9f6e1b1
Shagen Ogandzhanian <shagen.ogandzhanian@jetbrains.com>
2025-05-28 13:15:02 +0200

    DEV allowedHosts: all

--- compose/mpp/demo/webpack.config.d/make_no_source_maps.js
+++ compose/mpp/demo/webpack.config.d/make_no_source_maps.js
@@ -1,2 +1,11 @@
 config.devtool = undefined; // default is `eval-source-map`
 config.mode = 'none'; // default is `development` (for jsBrowserRun).
+
+
+config.devServer = {
+  ...(config.devServer || {}),
+  compress: true,
+  // WARNING: disableHostCheck is removed in Webpack 5.
+  // Use allowedHosts instead if needed.
+  allowedHosts: "all",
+};
