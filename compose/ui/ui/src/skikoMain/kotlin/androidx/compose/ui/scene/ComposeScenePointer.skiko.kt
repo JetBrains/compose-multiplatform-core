f6bfa3b128dc8952271888bed4d75afc9a357473
Oleksandr.Karpovich <a.n.karpovich@gmail.com>
2025-06-18 14:55:54 +0200

    make PointerEventResult val value internal

--- compose/ui/ui/src/skikoMain/kotlin/androidx/compose/ui/scene/ComposeScenePointer.skiko.kt
+++ compose/ui/ui/src/skikoMain/kotlin/androidx/compose/ui/scene/ComposeScenePointer.skiko.kt
@@ -160,7 +160,7 @@ internal fun PointerInputEvent(
  */
 @InternalComposeUiApi
 @JvmInline
-value class PointerEventResult internal constructor(val value: Int) {
+value class PointerEventResult internal constructor(internal val value: Int) {
 
     constructor(
         anyMovementConsumed: Boolean = false,
