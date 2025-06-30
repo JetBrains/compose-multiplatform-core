bfb1cbc0f0a3c9e1679400e18489337296bbdcbe
Ivan Matkov <ivan.matkov@jetbrains.com>
2025-06-30 16:07:35 +0200

    Mark `isTypedEvent` as `@InternalFoundationApi` instead of `internal` to temporary provide compatibility with existing usages

--- compose/foundation/foundation/src/webCommonW3C/kotlin/androidx/compose/foundation/text/TextFieldKeyInput.js.kt
+++ compose/foundation/foundation/src/webCommonW3C/kotlin/androidx/compose/foundation/text/TextFieldKeyInput.js.kt
@@ -16,7 +16,7 @@
 
 package androidx.compose.foundation.text
 
-import androidx.annotation.VisibleForTesting
+import androidx.compose.foundation.InternalFoundationApi
 import androidx.compose.ui.dom.domEventOrNull
 import androidx.compose.ui.input.key.KeyEvent
 import androidx.compose.ui.input.key.KeyEventType
@@ -25,7 +25,7 @@ import androidx.compose.ui.input.key.isMetaPressed
 import androidx.compose.ui.input.key.type
 import org.w3c.dom.events.KeyboardEvent
 
-@VisibleForTesting // TODO: Remove from public at all
+@InternalFoundationApi // TODO: Remove from public at all
 actual val KeyEvent.isTypedEvent: Boolean
     get() = type == KeyEventType.KeyDown
         && !isMetaPressed
