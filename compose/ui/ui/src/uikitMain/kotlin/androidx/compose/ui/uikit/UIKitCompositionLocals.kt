858238cb1813abf290f862d08d4841a0f4ecf4a9
Andrei Salavei <andrei.salavei@jetbrains.com>
2025-07-04 11:48:38 +0200

    Merge UIKit locals into one file

--- compose/ui/ui/src/uikitMain/kotlin/androidx/compose/ui/uikit/UIKitCompositionLocals.kt
+++ compose/ui/ui/src/uikitMain/kotlin/androidx/compose/ui/uikit/UIKitCompositionLocals.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2025 The Android Open Source Project
+ * Copyright 2024 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -19,6 +19,17 @@ package androidx.compose.ui.uikit
 import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.compose.ui.ExperimentalComposeUiApi
 import platform.UIKit.UIView
+import platform.UIKit.UIViewController
+
+/**
+ * Public value to get UIViewController of Compose window for library authors.
+ * Maybe useful for features, like VideoPlayer and Bottom menus.
+ * Please use it carefully and don't add or remove other views - check
+ * [androidx.compose.ui.interop.UIKitView] for those purposes.
+ */
+val LocalUIViewController = staticCompositionLocalOf<UIViewController> {
+    error("CompositionLocal UIViewController not provided")
+}
 
 /**
  * Public value to get top UIView of the current Compose window for library authors.
