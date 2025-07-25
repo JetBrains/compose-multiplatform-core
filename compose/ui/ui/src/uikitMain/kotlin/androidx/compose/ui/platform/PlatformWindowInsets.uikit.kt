d0577aa3eb8e99084eff53603d8f768961929850
svastven <vendula.svastalova@jetbrains.com>
2025-07-25 11:40:59 +0200

    Refactor PlatformWindowInsets to remove derivedStateOf usage for consistent and direct property getters

--- compose/ui/ui/src/uikitMain/kotlin/androidx/compose/ui/platform/PlatformWindowInsets.uikit.kt
+++ compose/ui/ui/src/uikitMain/kotlin/androidx/compose/ui/platform/PlatformWindowInsets.uikit.kt
@@ -16,7 +16,6 @@
 
 package androidx.compose.ui.platform
 
-import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.geometry.Rect
 import androidx.compose.ui.uikit.InterfaceOrientation
@@ -35,76 +34,61 @@ internal class UIKitWindowInsets: PlatformWindowInsets {
     private var sceneSize = mutableStateOf(IntSize.Zero)
     private var interfaceOrientation = mutableStateOf(InterfaceOrientation.Portrait)
 
-    private val _displayCutouts = derivedStateOf {
-        val orientation = interfaceOrientation.value
-        val safeAreaInsets = safeAreaInsets.value
-        val sceneSize = sceneSize.value
+    override val displayCutouts: List<Rect>
+        get() {
+            val orientation = interfaceOrientation.value
+            val safeAreaInsets = safeAreaInsets.value
+            val sceneSize = sceneSize.value
 
-        val hasCutout = when(orientation) {
-            InterfaceOrientation.Portrait -> safeAreaInsets.top > 0
-            InterfaceOrientation.PortraitUpsideDown -> safeAreaInsets.bottom > 0
-            InterfaceOrientation.LandscapeLeft -> safeAreaInsets.left > 0
-            InterfaceOrientation.LandscapeRight -> safeAreaInsets.right > 0
-        }
+            val hasCutout = when(orientation) {
+                InterfaceOrientation.Portrait -> safeAreaInsets.top > 0
+                InterfaceOrientation.PortraitUpsideDown -> safeAreaInsets.bottom > 0
+                InterfaceOrientation.LandscapeLeft -> safeAreaInsets.left > 0
+                InterfaceOrientation.LandscapeRight -> safeAreaInsets.right > 0
+            }
 
-        if (!hasCutout || sceneSize.width <= 0 || sceneSize.height <= 0) {
-            emptyList()
-        } else {
-            when (orientation) {
-                InterfaceOrientation.Portrait -> listOf(
-                    Rect(0f, 0f, sceneSize.width.toFloat(), safeAreaInsets.top.toFloat())
-                )
-                InterfaceOrientation.PortraitUpsideDown -> listOf(
-                    Rect(0f, sceneSize.height - safeAreaInsets.bottom.toFloat(), sceneSize.width.toFloat(), sceneSize.height.toFloat())
-                )
-                InterfaceOrientation.LandscapeLeft -> listOf(
-                    Rect(0f, 0f, safeAreaInsets.left.toFloat(), sceneSize.height.toFloat())
-                )
-                InterfaceOrientation.LandscapeRight -> listOf(
-                    Rect(sceneSize.width - safeAreaInsets.right.toFloat(), 0f, sceneSize.width.toFloat(), sceneSize.height.toFloat())
-                )
+            if (!hasCutout || sceneSize.width <= 0 || sceneSize.height <= 0) {
+                return emptyList()
+            } else {
+                return when (orientation) {
+                    InterfaceOrientation.Portrait -> listOf(
+                        Rect(0f, 0f, sceneSize.width.toFloat(), safeAreaInsets.top.toFloat())
+                    )
+                    InterfaceOrientation.PortraitUpsideDown -> listOf(
+                        Rect(0f, sceneSize.height - safeAreaInsets.bottom.toFloat(), sceneSize.width.toFloat(), sceneSize.height.toFloat())
+                    )
+                    InterfaceOrientation.LandscapeLeft -> listOf(
+                        Rect(0f, 0f, safeAreaInsets.left.toFloat(), sceneSize.height.toFloat())
+                    )
+                    InterfaceOrientation.LandscapeRight -> listOf(
+                        Rect(sceneSize.width - safeAreaInsets.right.toFloat(), 0f, sceneSize.width.toFloat(), sceneSize.height.toFloat())
+                    )
+                }
             }
         }
-    }
-
-    private val _displayCutout = derivedStateOf {
-        val orientation = interfaceOrientation.value
-        val safeAreaInsets = safeAreaInsets.value
-
-        when(orientation) {
-            InterfaceOrientation.Portrait -> PlatformInsets(top = safeAreaInsets.top)
-            InterfaceOrientation.PortraitUpsideDown -> PlatformInsets(bottom = safeAreaInsets.bottom)
-            InterfaceOrientation.LandscapeLeft -> PlatformInsets(left = safeAreaInsets.left)
-            InterfaceOrientation.LandscapeRight -> PlatformInsets(right = safeAreaInsets.right)
-        }
-    }
-    private val _ime = derivedStateOf {
-        PlatformInsets(bottom = keyboardOverlapHeight.value)
-    }
-    private val _mandatorySystemGesture = derivedStateOf {
-        val safeAreaInsets = safeAreaInsets.value
-        PlatformInsets(top = safeAreaInsets.top, bottom = safeAreaInsets.bottom)
-    }
-    private val _navigationBars = derivedStateOf {
-        PlatformInsets(bottom = safeAreaInsets.value.bottom)
-    }
-    private val _statusBars = derivedStateOf {
-        PlatformInsets(top = safeAreaInsets.value.top)
-    }
-    private val _tappableElement = derivedStateOf {
-        PlatformInsets(top = safeAreaInsets.value.top)
-    }
-
-    override val displayCutouts: List<Rect> get() = _displayCutouts.value
     override val captionBar: PlatformInsets get() = PlatformInsets.Zero
-    override val displayCutout: PlatformInsets get() = _displayCutout.value
-    override val ime: PlatformInsets get() = _ime.value
-    override val mandatorySystemGestures: PlatformInsets get() = _mandatorySystemGesture.value
-    override val navigationBars: PlatformInsets get() = _navigationBars.value
-    override val statusBars: PlatformInsets get() = _statusBars.value
+    override val displayCutout: PlatformInsets
+        get() {
+            val orientation = interfaceOrientation.value
+            val safeAreaInsets = safeAreaInsets.value
+
+            return when(orientation) {
+                InterfaceOrientation.Portrait -> PlatformInsets(top = safeAreaInsets.top)
+                InterfaceOrientation.PortraitUpsideDown -> PlatformInsets(bottom = safeAreaInsets.bottom)
+                InterfaceOrientation.LandscapeLeft -> PlatformInsets(left = safeAreaInsets.left)
+                InterfaceOrientation.LandscapeRight -> PlatformInsets(right = safeAreaInsets.right)
+            }
+        }
+    override val ime: PlatformInsets get() = PlatformInsets(bottom = keyboardOverlapHeight.value)
+    override val mandatorySystemGestures: PlatformInsets get() {
+        val safeAreaInsets = safeAreaInsets.value
+        return PlatformInsets(top = safeAreaInsets.top, bottom = safeAreaInsets.bottom)
+    }
+    override val navigationBars: PlatformInsets get() = PlatformInsets(bottom = safeAreaInsets.value.bottom)
+    override val statusBars: PlatformInsets get() = PlatformInsets(top = safeAreaInsets.value.top)
     override val systemBars: PlatformInsets get() = safeAreaInsets.value
     override val systemGestures: PlatformInsets get() = layoutMargins.value
-    override val tappableElement: PlatformInsets get() = _tappableElement.value
+    override val tappableElement: PlatformInsets get() = PlatformInsets(top = safeAreaInsets.value.top)
     override val waterfall: PlatformInsets get() = PlatformInsets.Zero
 
     fun updateInterfaceOrientation(orientation: InterfaceOrientation) {
