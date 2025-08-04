b6c1cad781722f5715a2e356ed557330cfd89e6e
Shagen Ogandzhanian <shagen.ogandzhanian@jetbrains.com>
2025-08-04 11:27:50 +0200

    Increase CanvasBasedWindow deprecation level to DeprecationLevel.ERROR

--- compose/ui/ui/src/webCommonW3C/kotlin/androidx/compose/ui/window/ComposeWindow.w3c.kt
+++ compose/ui/ui/src/webCommonW3C/kotlin/androidx/compose/ui/window/ComposeWindow.w3c.kt
@@ -643,7 +643,10 @@ private const val defaultCanvasElementId = "ComposeTarget"
  * This can be turned off by setting [applyDefaultStyles] to false.
  */
 @ExperimentalComposeUiApi
-@Deprecated("CanvasBasedWindow doesn't support HTML interop via WebElementView API and it doesn't support A11Y feature. Use ComposeViewport API instead")
+@Deprecated(
+message = "CanvasBasedWindow doesn't support HTML interop via WebElementView API and it doesn't support A11Y feature. Use ComposeViewport API instead",
+replaceWith = ReplaceWith("ComposeViewport"),
+level = DeprecationLevel.ERROR)
 fun CanvasBasedWindow(
     title: String? = null,
     canvasElementId: String = defaultCanvasElementId,
