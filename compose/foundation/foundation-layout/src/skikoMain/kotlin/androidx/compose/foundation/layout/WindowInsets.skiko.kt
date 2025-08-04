fee5a1ecdd8df004ef039655885017d0348fb94a
Ivan Matkov <ivan.matkov@jetbrains.com>
2025-08-04 14:51:18 +0200

    Align actual signatures
    Change-Id: I1dc928564eb9c78f5613a6f52ea655a989bf7605

--- compose/foundation/foundation-layout/src/skikoMain/kotlin/androidx/compose/foundation/layout/WindowInsets.skiko.kt
+++ compose/foundation/foundation-layout/src/skikoMain/kotlin/androidx/compose/foundation/layout/WindowInsets.skiko.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2023 The Android Open Source Project
+ * Copyright 2025 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,11 +14,11 @@
  * limitations under the License.
  */
 
-package androidx.compose.material3
+package androidx.compose.foundation.layout
 
-internal actual fun Int.toLocalString(
-    minDigits: Int,
-    maxDigits: Int,
-    isGroupingUsed: Boolean,
-    locale: CalendarLocale?,
-): String = toString().padStart(minDigits, '0')
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Path
+
+// TODO: https://youtrack.jetbrains.com/issue/CMP-8684
+actual val WindowInsets.Companion.cutoutPath: Path?
+    @Composable get() = null
