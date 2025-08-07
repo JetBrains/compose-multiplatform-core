2947ffa26a7e961bc265cfeb97830b59d6aafcec
svastven <vendula.svastalova@jetbrains.com>
2025-08-07 13:15:42 +0200

    Add WindowInsetsTest to verify no recomposition on IME insets changes

--- compose/ui/ui/src/skikoTest/kotlin/androidx/compose/ui/layout/WindowInsetsTest.kt
+++ compose/ui/ui/src/skikoTest/kotlin/androidx/compose/ui/layout/WindowInsetsTest.kt
@@ -22,39 +22,146 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.safeDrawingPadding
 import androidx.compose.material.TextField
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.FocusRequester
-import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.geometry.Rect
+import androidx.compose.ui.platform.LocalPlatformWindowInsets
+import androidx.compose.ui.platform.PlatformInsets
+import androidx.compose.ui.platform.PlatformWindowInsets
 import androidx.compose.ui.test.ExperimentalTestApi
-import androidx.compose.ui.test.runComposeUiTest
+import androidx.compose.ui.test.runSkikoComposeUiTest
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertNotEquals
 
 @OptIn(ExperimentalTestApi::class)
 class WindowInsetsTest {
 
     @Test
-    fun composableDoesNotRecomposeOnWindowInsetsImeChange() = runComposeUiTest {
+    fun composableDoesNotRecomposeOnWindowInsetsImeChange() = runSkikoComposeUiTest {
         var compositionCount = 0
+        val imeInsets = mutableStateOf(PlatformInsets.Zero)
+        var initialTextFieldBounds: Rect
+        var textFieldBounds = Rect.Zero
 
         setContent {
-            val focusRequester = remember { FocusRequester() }
-            Column(modifier = Modifier.fillMaxSize().safeDrawingPadding().imePadding()){
-                Spacer(modifier = Modifier.weight(1f))
-                TextField(
-                    "",
-                    {},
-                    Modifier.focusRequester(focusRequester)
-                )
-                compositionCount++
-            }
-            LaunchedEffect(Unit) {
-                focusRequester.requestFocus()
-            }
+            TestContent(
+                imeInsets,
+                { compositionCount++ },
+                { textFieldBounds = it }
+            )
         }
 
+        initialTextFieldBounds = textFieldBounds
+
+        assertNotEquals(Rect.Zero, textFieldBounds)
+        assertEquals(1, compositionCount)
+
+        imeInsets.value = PlatformInsets(bottom = 100)
+
+        waitForIdle()
+
+        assertEquals(Rect(initialTextFieldBounds.left, initialTextFieldBounds.top - 100, initialTextFieldBounds.right, initialTextFieldBounds.bottom - 100), textFieldBounds)
         assertEquals(1, compositionCount)
     }
+
+    @Test
+    fun composableDoesNotRecomposeOnWindowInsetsImeConsecutiveChange() = runSkikoComposeUiTest {
+        var compositionCount = 0
+        val imeInsets = mutableStateOf(PlatformInsets.Zero)
+        val maxBottomInset = 100
+        var initialTextFieldBounds: Rect
+        var textFieldBounds = Rect.Zero
+
+        setContent {
+            TestContent(
+                imeInsets,
+                { compositionCount++ },
+                { textFieldBounds = it }
+            )
+        }
+
+        initialTextFieldBounds = textFieldBounds
+
+        assertNotEquals(Rect.Zero, textFieldBounds)
+        assertEquals(1, compositionCount)
+
+        for (i in 1..maxBottomInset) {
+            imeInsets.value = PlatformInsets(bottom = i)
+            waitForIdle()
+        }
+
+        assertEquals(Rect(initialTextFieldBounds.left, initialTextFieldBounds.top - maxBottomInset, initialTextFieldBounds.right, initialTextFieldBounds.bottom - maxBottomInset), textFieldBounds)
+        assertEquals(1, compositionCount)
+    }
+
+    @Test
+    fun composableDoesNotRecomposeOnWindowInsetsImeChangeAndReset() = runSkikoComposeUiTest {
+        var compositionCount = 0
+        val imeInsets = mutableStateOf(PlatformInsets.Zero)
+        val maxBottomInset = 100
+        var initialTextFieldBounds: Rect
+        var textFieldBounds = Rect.Zero
+
+        setContent {
+            TestContent(
+                imeInsets,
+                { compositionCount++ },
+                { textFieldBounds = it }
+            )
+        }
+
+        initialTextFieldBounds = textFieldBounds
+
+        assertNotEquals(Rect.Zero, textFieldBounds)
+        assertEquals(1, compositionCount)
+
+        for (i in 1..maxBottomInset) {
+            imeInsets.value = PlatformInsets(bottom = i)
+            waitForIdle()
+        }
+
+        for (i in 1..maxBottomInset) {
+            imeInsets.value = PlatformInsets(bottom = maxBottomInset - i)
+            waitForIdle()
+        }
+
+        assertEquals(initialTextFieldBounds, textFieldBounds)
+        assertEquals(1, compositionCount)
+    }
+
+
+}
+
+@Composable
+private fun TestContent(
+    imeInsets: MutableState<PlatformInsets>,
+    onComposition: () -> Unit,
+    onTextFieldPositioned: (Rect) -> Unit
+) {
+    CompositionLocalProvider(LocalPlatformWindowInsets provides TestWindowInsets(imeInsets = imeInsets)) {
+        Column(modifier = Modifier.fillMaxSize().safeDrawingPadding().imePadding()){
+            Spacer(modifier = Modifier.weight(1f))
+            TextField(
+                value ="",
+                onValueChange = {},
+                modifier = Modifier.onGloballyPositioned { onTextFieldPositioned(it.boundsInRoot()) }
+            )
+            onComposition()
+        }
+    }
+}
+
+private fun TestWindowInsets(
+    imeInsets: MutableState<PlatformInsets>
+): PlatformWindowInsets = object : PlatformWindowInsets {
+    override val ime: PlatformInsets get() = PlatformInsets(
+        getBottom = { imeInsets.value.bottom },
+        getTop = { imeInsets.value.top },
+        getLeft = { imeInsets.value.left },
+        getRight = { imeInsets.value.right }
+    )
 }
