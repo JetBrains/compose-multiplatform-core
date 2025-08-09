2913cce0f51f0ccb29cf08751f13c966fa3937ba
svastven <vendula.svastalova@jetbrains.com>
2025-08-10 01:13:15 +0200

    Add tests for WindowInsets.displayCutout for various interface orientations

--- compose/ui/ui/src/uikitInstrumentedTest/kotlin/androidx/compose/ui/layout/WindowInsetsPaddingTest.kt
+++ compose/ui/ui/src/uikitInstrumentedTest/kotlin/androidx/compose/ui/layout/WindowInsetsPaddingTest.kt
@@ -16,20 +16,42 @@
 
 package androidx.compose.ui.layout
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.displayCutout
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.safeDrawingPadding
+import androidx.compose.foundation.layout.windowInsetsPadding
+import androidx.compose.material.Text
 import androidx.compose.material.TextField
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.test.UIKitInstrumentedTest
 import androidx.compose.ui.test.runUIKitInstrumentedTest
+import androidx.compose.ui.test.utils.DpRectZero
+import androidx.compose.ui.uikit.ComposeUIViewControllerConfiguration
+import androidx.compose.ui.unit.DpOffset
+import androidx.compose.ui.unit.DpRect
+import androidx.compose.ui.unit.DpSize
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.toDpRect
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import kotlinx.cinterop.ExperimentalForeignApi
+import platform.UIKit.UIInterfaceOrientationMask
+import platform.UIKit.UIInterfaceOrientationMaskLandscapeLeft
+import platform.UIKit.UIInterfaceOrientationMaskLandscapeRight
+import platform.UIKit.UIViewController
+import platform.UIKit.attemptRotationToDeviceOrientation
 
 class WindowInsetsPaddingTest {
     @Test
@@ -53,5 +75,72 @@ class WindowInsetsPaddingTest {
         }
 
         assertEquals(1, compositionCount)
+    }
+
+    @OptIn(ExperimentalForeignApi::class)
+    @Test
+    fun testDisplayCutoutPadding_InterfaceOrientationLandscapeLeft() = runUIKitInstrumentedTest {
+        var boxRect = DpRectZero()
+
+        setContent(interfaceOrientations = UIInterfaceOrientationMaskLandscapeLeft) {
+            Box(
+                Modifier
+                    .fillMaxSize()
+                    .windowInsetsPadding(WindowInsets.displayCutout)
+                    .background(Color.Red)
+                    .onGloballyPositioned({ boxRect = it.boundsInWindow().toDpRect(density) })
+            ) {
+                Text("TEXT")
+            }
+        }
+
+        assertEquals(
+            DpRect(DpOffset.Zero, size = DpSize(
+                screenSize.width - with(density) { hostingViewController.safeAreaInsets.right.toDp() },
+                screenSize.height
+            )),
+            boxRect
+        )
+    }
+
+    @OptIn(ExperimentalForeignApi::class)
+    @Test
+    fun testDisplayCutoutPadding_InterfaceOrientationLandscapeRight() = runUIKitInstrumentedTest {
+        var boxRect = DpRectZero()
+
+        setContent(interfaceOrientations = UIInterfaceOrientationMaskLandscapeRight) {
+            Box(
+                Modifier
+                    .fillMaxSize()
+                    .windowInsetsPadding(WindowInsets.displayCutout)
+                    .background(Color.Red)
+                    .onGloballyPositioned({ boxRect = it.boundsInWindow().toDpRect(density) })
+            ) {
+                Text("TEXT")
+            }
+        }
+
+        val xOffset = with(density) { hostingViewController.safeAreaInsets.left.toDp() }
+
+        assertEquals(
+            DpRect(
+                DpOffset(xOffset, 0.dp),
+                size = DpSize(screenSize.width - xOffset, screenSize.height)
+            ),
+            boxRect
+        )
+    }
+}
+
+private fun UIKitInstrumentedTest.setContent(
+    configure: ComposeUIViewControllerConfiguration.() -> Unit = {},
+    interfaceOrientations: UIInterfaceOrientationMask,
+    content: @Composable () -> Unit
+) {
+    setContent(configure, content)
+    if (appDelegate.supportedInterfaceOrientations != interfaceOrientations) {
+        appDelegate.supportedInterfaceOrientations = interfaceOrientations
+        UIViewController.attemptRotationToDeviceOrientation()
+        delay(1000)
     }
 }
