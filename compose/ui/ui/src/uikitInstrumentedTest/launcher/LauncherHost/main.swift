87dfe47c1294fb7389bbbdb1428279b14051685b
Andrei Salavei <andrei.salavei@jetbrains.com>
2025-08-14 15:58:54 +0200

    revert launcher app

--- compose/ui/ui/src/uikitInstrumentedTest/launcher/LauncherHost/main.swift
+++ compose/ui/ui/src/uikitInstrumentedTest/launcher/LauncherHost/main.swift
@@ -14,13 +14,19 @@
  * limitations under the License.
  */
 
-import SwiftUI
+import UIKit
 
-@main
-struct MyApp: App {
-    var body: some Scene {
-        WindowGroup {
-            Color.orange
-        }
+class AppDelegate: NSObject, UIApplicationDelegate {
+    
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = UIViewController()
+        window?.rootViewController?.view.backgroundColor = .orange
+        window?.makeKeyAndVisible()
+        return true
     }
 }
+
+UIApplicationMain(CommandLine.argc, CommandLine.unsafeArgv, nil, NSStringFromClass(AppDelegate.self))
