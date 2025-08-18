d35cfd6f7bc90a5fe924dcd51502707bf33cdc12
Andrei Salavei <andrei.salavei@jetbrains.com>
2025-08-18 16:24:51 +0200

    Add expect functions

--- compose/ui/ui/src/desktopMain/kotlin/androidx/compose/ui/input/pointer/util/PlatformVelocityTracker.desktop.kt
+++ compose/ui/ui/src/desktopMain/kotlin/androidx/compose/ui/input/pointer/util/PlatformVelocityTracker.desktop.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2023 The Android Open Source Project
+ * Copyright 2025 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,6 +14,7 @@
  * limitations under the License.
  */
 
-package androidx.compose.ui.platform
+package androidx.compose.ui.input.pointer.util
 
-internal actual var PlatformInsetsConfig: InsetsConfig = ZeroInsetsConfig
+/** Create an instance of the platform-specific velocity tracker. */
+internal actual fun PlatformVelocityTracker(): PlatformVelocityTracker = DefaultVelocityTracker()
