21be9ae45c28f13863823e8de347b9c0fa1ebd33
Andrei Salavei <andrei.salavei@jetbrains.com>
2025-08-20 15:13:38 +0200

    Update invalidations logic

--- compose/ui/ui/src/uikitMain/kotlin/androidx/compose/ui/platform/Accessibility.uikit.kt
+++ compose/ui/ui/src/uikitMain/kotlin/androidx/compose/ui/platform/Accessibility.uikit.kt
@@ -1137,7 +1137,6 @@ internal class AccessibilityMediator(
             // Will exit on CancellationException from within await on `invalidationChannel.receive()`
             // when [job] is cancelled
             while (true) {
-                hasPendingInvalidations = false
                 invalidationChannel.receive()
                 hasPendingInvalidations = true
 
@@ -1171,6 +1170,7 @@ internal class AccessibilityMediator(
                     // iOS Accessibility Engine will ignore them.
                     delay(100)
                 }
+                hasPendingInvalidations = false
             }
         }
     }
