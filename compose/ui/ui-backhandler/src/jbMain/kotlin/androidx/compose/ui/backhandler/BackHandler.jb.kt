ea50af7c1e0213d5c95216dec3ddf6cf2a185b93
Konstantin Tskhovrebov <konstantin.tskhovrebov@jetbrains.com>
2025-10-01 12:32:17 +0200

    Refactor BackHandler implementation to use NavigationBackHandler and refine progress handling logic

--- compose/ui/ui-backhandler/src/jbMain/kotlin/androidx/compose/ui/backhandler/BackHandler.jb.kt
+++ compose/ui/ui-backhandler/src/jbMain/kotlin/androidx/compose/ui/backhandler/BackHandler.jb.kt
@@ -19,10 +19,26 @@
 package androidx.compose.ui.backhandler
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.navigationevent.NavigationEvent
+import androidx.navigationevent.NavigationEventInfo
+import androidx.navigationevent.NavigationEventTransitionState
 import androidx.navigationevent.compose.LocalNavigationEventDispatcherOwner
+import androidx.navigationevent.compose.NavigationBackHandler
+import androidx.navigationevent.compose.rememberNavigationEventState
 import kotlinx.coroutines.CancellationException
+import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.consumeAsFlow
+import kotlinx.coroutines.flow.filterIsInstance
+import kotlinx.coroutines.launch
 
 @Deprecated("Use NavigationEventHandler instead")
 @ExperimentalComposeUiApi
@@ -31,33 +47,73 @@ actual fun PredictiveBackHandler(
     enabled: Boolean,
     onBack: suspend (progress: Flow<BackEventCompat>) -> Unit
 ) {
-    LocalNavigationEventDispatcherOwner.current ?: return
-    /*
-    TODO: https://youtrack.jetbrains.com/issue/CMP-8937
-    NavigationEventHandler(enabled) { progress ->
-        val compatProgress = progress.map { navEvent ->
-            val swipeEdge = when (navEvent.swipeEdge) {
-                NavigationEventSwipeEdge.Left -> BackEventCompat.EDGE_LEFT
-                NavigationEventSwipeEdge.Right -> BackEventCompat.EDGE_RIGHT
-                else -> 0
-            }
-            BackEventCompat(navEvent.touchX, navEvent.touchY, navEvent.progress, swipeEdge)
-        }
-        onBack(compatProgress)
+    val owner = LocalNavigationEventDispatcherOwner.current ?: return
+    val dispatcher = owner.navigationEventDispatcher
+    val coroutineScope = rememberCoroutineScope()
+
+    var progressChannel: Channel<BackEventCompat>? by remember(onBack) {
+        mutableStateOf(null)
     }
-    */
+
+    fun getActiveProgressChannel(): Channel<BackEventCompat> {
+        val currentProgressChannel = progressChannel
+        if (currentProgressChannel == null) {
+            val progress = Channel<BackEventCompat>()
+            progressChannel = progress
+            coroutineScope.launch {
+                onBack(progress.consumeAsFlow())
+            }
+            return progress
+        } else {
+            return currentProgressChannel
+        }
+    }
+
+    LaunchedEffect(enabled) {
+        if (enabled) {
+            dispatcher.transitionState
+                .filterIsInstance<NavigationEventTransitionState.InProgress>()
+                .collect {
+                    val navEvent = it.latestEvent
+                    val swipeEdge = when (navEvent.swipeEdge) {
+                        NavigationEvent.EDGE_RIGHT -> BackEventCompat.EDGE_RIGHT
+                        else -> BackEventCompat.EDGE_LEFT
+                    }
+                    val event = BackEventCompat(
+                        navEvent.touchX, navEvent.touchY, navEvent.progress, swipeEdge
+                    )
+                    getActiveProgressChannel().send(event)
+                }
+        }
+    }
+
+    NavigationBackHandler(
+        state = rememberNavigationEventState(NavigationEventInfo.None),
+        isBackEnabled = enabled,
+        onBackCancelled = {
+            getActiveProgressChannel().close(CancellationException("Cancelled"))
+            progressChannel = null
+        },
+        onBackCompleted = {
+            getActiveProgressChannel().close()
+            progressChannel = null
+        }
+    )
+    DisposableEffect(Unit) {
+        onDispose {
+            progressChannel?.close(CancellationException("Disposed"))
+            progressChannel = null
+        }
+    }
 }
 
 @Deprecated("Use NavigationEventHandler instead")
 @ExperimentalComposeUiApi
 @Composable
 actual fun BackHandler(enabled: Boolean, onBack: () -> Unit) {
-    PredictiveBackHandler(enabled) { progress ->
-        try {
-            progress.collect { /*ignore*/ }
-            onBack()
-        } catch (e: CancellationException) {
-            //ignore
-        }
-    }
+    NavigationBackHandler(
+        state = rememberNavigationEventState(NavigationEventInfo.None),
+        isBackEnabled = enabled,
+        onBackCompleted = onBack
+    )
 }
