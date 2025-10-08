8a7def507ecdd7af0fd7333056a3e6aaef2bcc0a
Ivan Matkov <ivan.matkov@jetbrains.com>
2025-10-08 19:34:18 +0200

    Revert test commonization for adaptive-layout module
    Change-Id: If1108bb6e46c6e6affedfbdcb92a241c9e889054

--- compose/material3/adaptive/adaptive-layout/src/androidUnitTest/kotlin/androidx/compose/material3/adaptive/layout/PaneMotionTest.kt
+++ compose/material3/adaptive/adaptive-layout/src/androidUnitTest/kotlin/androidx/compose/material3/adaptive/layout/PaneMotionTest.kt
@@ -38,12 +38,14 @@ import androidx.compose.material3.adaptive.layout.PaneMotion.Companion.NoMotion
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.IntSize
-import androidx.kruth.assertThat
-import androidx.kruth.assertWithMessage
-import kotlin.test.Ignore
-import kotlin.test.Test
+import com.google.common.truth.Truth.assertThat
+import com.google.common.truth.Truth.assertWithMessage
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
 
 @OptIn(ExperimentalMaterial3AdaptiveApi::class)
+@RunWith(JUnit4::class)
 class PaneMotionTest {
     @Test
     fun test_allThreePaneMotions() {
@@ -58,7 +60,6 @@ class PaneMotionTest {
         }
     }
 
-    @Ignore // TODO: https://youtrack.jetbrains.com/issue/CMP-7979
     @Test
     fun test_allDefaultPaneMotionTransitions() {
         NoMotion.assertTransitions(EnterTransition.None, ExitTransition.None)
--- compose/material3/adaptive/adaptive-layout/src/androidUnitTest/kotlin/androidx/compose/material3/adaptive/layout/ThreePaneScaffoldValueTest.kt
+++ compose/material3/adaptive/adaptive-layout/src/androidUnitTest/kotlin/androidx/compose/material3/adaptive/layout/ThreePaneScaffoldValueTest.kt
@@ -18,10 +18,13 @@ package androidx.compose.material3.adaptive.layout
 
 import androidx.compose.material3.adaptive.ExperimentalMaterial3AdaptiveApi
 import androidx.compose.ui.Alignment
-import androidx.kruth.assertThat
-import kotlin.test.Test
+import com.google.common.truth.Truth.assertThat
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
 
 @OptIn(ExperimentalMaterial3AdaptiveApi::class)
+@RunWith(JUnit4::class)
 class ThreePaneScaffoldValueTest {
     @Test
     fun calculateWithoutHistory_onePaneLayout_noDestination() {
