79bd64516221b131c45c3bf9c46c5af1d77feec8
Igor Demin <igor.demin@jetbrains.com>
2025-10-10 15:47:48 +0200

    Fix `publishComposeJbToMavenLocal`

--- buildSrc/public/src/main/kotlin/org/jetbrains/androidx/build/ComposePlatforms.kt
+++ buildSrc/public/src/main/kotlin/org/jetbrains/androidx/build/ComposePlatforms.kt
@@ -14,8 +14,7 @@ import org.gradle.api.Project
 enum class ComposePlatforms(vararg val alternativeNames: String) {
     KotlinMultiplatform("Common", "Metadata"),
     Desktop("Jvm"),
-    AndroidDebug("Android"),
-    AndroidRelease("Android"),
+    Android("Android"),
     Js("Web"),
     WasmJs("Web"),
     MacosX64("Macos"),
@@ -50,8 +49,7 @@ enum class ComposePlatforms(vararg val alternativeNames: String) {
     companion object {
         val JVM_BASED = EnumSet.of(
             Desktop,
-            AndroidDebug,
-            AndroidRelease
+            Android
         )
 
         val IOS = EnumSet.of(
@@ -74,8 +72,7 @@ enum class ComposePlatforms(vararg val alternativeNames: String) {
         )
 
         val ANDROID = EnumSet.of(
-            AndroidDebug,
-            AndroidRelease
+            Android
         )
 
         val WINDOWS_NATIVE = EnumSet.of(
