808f1b35d8a80871b14114d868db7f3de8676171
Andrei Salavei <andrei.salavei@jetbrains.com>
2025-10-14 00:57:09 +0200

    Print devices

--- .jb-ciscripts/PullRequests/UIKitInstrumentedTests.sh
+++ .jb-ciscripts/PullRequests/UIKitInstrumentedTests.sh
@@ -11,6 +11,8 @@ killall Simulator
 DEVICES=$(xcrun simctl list DEVICES --json)
 TEST_DEVICE="iPhone 16"
 
+echo $DEVICES
+
 # This assumes you have jq installed; you can also parse manually if not
 if ! command -v jq &> /dev/null; then
   echo "Error: jq is not installed"
@@ -25,12 +27,6 @@ defaults export com.apple.iphonesimulator - > "$PREF_PLIST"
 echo "$DEVICES" | jq -r '.DEVICES | to_entries[] | select(.key | startswith("com.apple.CoreSimulator.SimRuntime.iOS")) | .value[] | "\(.udid)"' | while read -r UUID; do
     /usr/libexec/PlistBuddy -c "Set :DevicePreferences:$UUID:ConnectHardwareKeyboard false" "$PREF_PLIST" 2>/dev/null || \
     /usr/libexec/PlistBuddy -c "Add :DevicePreferences:$UUID:ConnectHardwareKeyboard bool false" "$PREF_PLIST"
-
-    xcrun simctl spawn $UUID defaults write com.apple.Accessibility AccessibilityEnabled -bool true
-    xcrun simctl spawn $UUID defaults write com.apple.Accessibility ApplicationAccessibilityEnabled -bool true
-    xcrun simctl spawn $UUID defaults write com.apple.Accessibility AutomationEnabled -bool true
-    # Restart SpringBoard (so system services pick up the change)
-    xcrun simctl spawn $UUID launchctl stop com.apple.SpringBoard
 done
 
 # Import back the modified plist
