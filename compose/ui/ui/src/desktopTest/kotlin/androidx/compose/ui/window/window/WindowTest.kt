0537ee4c991149dcddf84d3b067c4b87c1c0a354
Alexander Maryanovsky <alexander.maryanovsky@jetbrains.com>
2025-10-22 13:06:44 +0000

    Try fixing `undecorated resizable window with unspecified size`

--- compose/ui/ui/src/desktopTest/kotlin/androidx/compose/ui/window/window/WindowTest.kt
+++ compose/ui/ui/src/desktopTest/kotlin/androidx/compose/ui/window/window/WindowTest.kt
@@ -589,10 +589,8 @@ class WindowTest {
     }
 
     @Test
-    fun `undecorated resizable window with unspecified size`() = runApplicationTest(
-        useDelay = true
-    ) {
-        var window: ComposeWindow? = null
+    fun `undecorated resizable window with unspecified size`() = runApplicationTest {
+        lateinit var window: ComposeWindow
 
         launchTestApplication {
             Window(
@@ -607,8 +605,9 @@ class WindowTest {
         }
 
         awaitIdle()
-        assertEquals(32, window?.width)
-        assertEquals(32, window?.height)
+        window.renderImmediately()
+        assertEquals(32, window.width)
+        assertEquals(32, window.height)
     }
 
     @Test
