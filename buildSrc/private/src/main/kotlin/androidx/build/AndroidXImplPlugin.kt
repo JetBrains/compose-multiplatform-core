c6251300f43e959f477f8410869725dffbf7b14a
Ivan Matkov <ivan.matkov@jetbrains.com>
2025-11-03 16:44:42 +0100

    Revert "Move to a new non deprecated way to set JavaCompile target version"
    This reverts commit 2667cb6590895d57e28901e154c7bce272e0ad12.

--- buildSrc/private/src/main/kotlin/androidx/build/AndroidXImplPlugin.kt
+++ buildSrc/private/src/main/kotlin/androidx/build/AndroidXImplPlugin.kt
@@ -439,6 +439,10 @@ abstract class AndroidXImplPlugin @Inject constructor() : Plugin<Project> {
             project.extensions.getByType<KotlinMultiplatformExtension>().apply {
                 targets.withType<KotlinMultiplatformAndroidLibraryTarget>().configureEach { t ->
                     t.compilations.configureEach { compilation ->
+                        // Replace with compilation.compileJavaTaskProvider?.configure {}
+                        // when b/438995010 is fixed
+                        @Suppress("DEPRECATION")
+                        compilation.compilerOptions.configure { jvmTarget.set(defaultJvmTarget) }
                         compilation.compileTaskProvider.configure {
                             it.compilerOptions.jvmTarget.set(defaultJvmTarget)
                         }
@@ -726,13 +730,6 @@ abstract class AndroidXImplPlugin @Inject constructor() : Plugin<Project> {
         }
 
         kotlinMultiplatformAndroidComponentsExtension.onVariants { variant ->
-            @Suppress("UnstableApiUsage")
-            variant.configureJavaCompileTask { compile ->
-                val defaultTargetJavaVersion =
-                    getDefaultTargetJavaVersion(androidXExtension.type, project.name).toString()
-                compile.sourceCompatibility = defaultTargetJavaVersion
-                compile.targetCompatibility = defaultTargetJavaVersion
-            }
             project.configureProjectForApiTasks(
                 AndroidMultiplatformApiTaskConfig(variant),
                 androidXExtension,
