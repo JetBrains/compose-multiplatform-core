4ce3427047cdb2f99d16881abad9d35714090b6e
Konstantin <konstantin.tskhovrebov@jetbrains.com>
2025-11-05 20:59:36 +0100

    Hot fix: remove navigation event input when desktop Component Detached

--- compose/ui/ui/src/desktopMain/kotlin/androidx/compose/ui/scene/ComposeSceneMediator.desktop.kt
+++ compose/ui/ui/src/desktopMain/kotlin/androidx/compose/ui/scene/ComposeSceneMediator.desktop.kt
@@ -590,7 +590,7 @@ internal class ComposeSceneMediator(
     fun onComponentDetached() {
         isComponentAttached = false
         architectureComponentsOwner.navigationEventDispatcherOwner
-            .navigationEventDispatcher.addInput(navigationEventInput)
+            .navigationEventDispatcher.removeInput(navigationEventInput)
         scene.focusManager.releaseFocus()
     }
 
