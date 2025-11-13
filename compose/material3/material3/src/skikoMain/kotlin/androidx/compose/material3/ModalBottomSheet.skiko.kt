6f6912bf33610155b8c38aa9da8428dad9efd808
svastven <vendula.svastalova@jetbrains.com>
2025-11-13 23:42:56 +0100

    Disable useSoftwareKeyboardInset in ModalBottomSheetDialog properties

--- compose/material3/material3/src/skikoMain/kotlin/androidx/compose/material3/ModalBottomSheet.skiko.kt
+++ compose/material3/material3/src/skikoMain/kotlin/androidx/compose/material3/ModalBottomSheet.skiko.kt
@@ -76,6 +76,7 @@ internal actual fun ModalBottomSheetDialog(
             dismissOnClickOutside = properties.shouldDismissOnClickOutside,
             usePlatformDefaultWidth = false,
             usePlatformInsets = false,
+            useSoftwareKeyboardInset = false,
             scrimColor = Color.Transparent,
         ),
         content = content
