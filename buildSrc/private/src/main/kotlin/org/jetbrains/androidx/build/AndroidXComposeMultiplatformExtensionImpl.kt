b57dab1699a2b1a071d1540a7569f154991be56b
Igor Demin <igor.demin@jetbrains.com>
2025-12-11 00:03:41 +0100

    Restore lint-checks

--- buildSrc/private/src/main/kotlin/org/jetbrains/androidx/build/AndroidXComposeMultiplatformExtensionImpl.kt
+++ buildSrc/private/src/main/kotlin/org/jetbrains/androidx/build/AndroidXComposeMultiplatformExtensionImpl.kt
@@ -44,8 +44,9 @@ open class AndroidXComposeMultiplatformExtensionImpl @Inject constructor(
     val project: Project
 ) : AndroidXComposeMultiplatformExtension() {
     init {
-        project.validateMultiplatformPluginHasNotBeenApplied()
-        project.plugins.apply(KotlinMultiplatformPluginWrapper::class.java)
+        if (!project.plugins.hasPlugin(KotlinMultiplatformPluginWrapper::class.java)) {
+            project.plugins.apply(KotlinMultiplatformPluginWrapper::class.java)
+        }
     }
 
     private val multiplatformExtension =
