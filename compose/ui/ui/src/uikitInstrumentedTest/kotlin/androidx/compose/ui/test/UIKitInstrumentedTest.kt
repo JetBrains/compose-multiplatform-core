90dae86658a42dfa55b0e8a0528ba1fb702807aa
Andrei Salavei <andrei.salavei@jetbrains.com>
2026-01-22 12:07:39 +0100

    Remove print this

--- compose/ui/ui/src/uikitInstrumentedTest/kotlin/androidx/compose/ui/test/UIKitInstrumentedTest.kt
+++ compose/ui/ui/src/uikitInstrumentedTest/kotlin/androidx/compose/ui/test/UIKitInstrumentedTest.kt
@@ -510,8 +510,9 @@ internal class MockAppDelegate: NSObject(), UIApplicationDelegateProtocol {
         application: UIApplication,
         supportedInterfaceOrientationsForWindow: UIWindow?
     ): UIInterfaceOrientationMask {
-        println(">> Accessing application mask!!!!!")
-        println(">> $this")
+         println("Works?")
+//        println(">>>> $this")
+
         return supportedInterfaceOrientations
     }
 }
