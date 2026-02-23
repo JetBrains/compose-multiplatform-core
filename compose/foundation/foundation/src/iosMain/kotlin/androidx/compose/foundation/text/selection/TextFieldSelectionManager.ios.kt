3bc9a198e3c8f7f2bab9770f6b25b2ee620e935b
Andrei Salavei <andrei.salavei@jetbrains.com>
2026-02-23 17:09:46 +0100

    Minor fixes

--- compose/foundation/foundation/src/iosMain/kotlin/androidx/compose/foundation/text/selection/TextFieldSelectionManager.ios.kt
+++ compose/foundation/foundation/src/iosMain/kotlin/androidx/compose/foundation/text/selection/TextFieldSelectionManager.ios.kt
@@ -64,7 +64,7 @@ internal actual fun Modifier.textFieldMagnifier(manager: TextFieldSelectionManag
                 }
             },
             color = color.handleColor, // align magnifier border color with selection handleColor
-            hapticFeedback = manager.hapticFeedBack
+            hapticFeedback = manager.hapticFeedBack,
         )
     }
 }
