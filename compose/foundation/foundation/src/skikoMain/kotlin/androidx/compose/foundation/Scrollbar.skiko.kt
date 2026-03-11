a6b79c236e76132a49082c47b99c4d6eec97cb0d
svastven <vendula.svastalova@jetbrains.com>
2026-03-11 19:20:58 +0100

    Replace resetPointerInputHandler with re-invoke of the handler

--- compose/foundation/foundation/src/skikoMain/kotlin/androidx/compose/foundation/Scrollbar.skiko.kt
+++ compose/foundation/foundation/src/skikoMain/kotlin/androidx/compose/foundation/Scrollbar.skiko.kt
@@ -891,9 +891,12 @@ private class ScrollbarDragModifierNode(
     private var draggedInteraction: MutableState<DragInteraction.Start?>,
     private var sliderAdapter: SliderAdapter,
 ) : DelegatingNode() {
+    private val pointerInputEventHandlerResetTick = mutableStateOf(0)
+
     private val pointerInputNode = delegate(
         SuspendingPointerInputModifierNode(
             pointerInputEventHandler = {
+                pointerInputEventHandlerResetTick.value
                 awaitEachGesture {
                     val down = awaitFirstDown(requireUnconsumed = false)
                     val interaction = DragInteraction.Start()
@@ -921,23 +924,23 @@ private class ScrollbarDragModifierNode(
         draggedInteraction: MutableState<DragInteraction.Start?>,
         sliderAdapter: SliderAdapter,
     ) {
-        var needsReset = false
+        var pointerInputEventHandlerNeedsResetTick = false
 
         if (this.interactionSource != interactionSource) {
             this.interactionSource = interactionSource
-            needsReset = true
+            pointerInputEventHandlerNeedsResetTick = true
         }
         if (this.draggedInteraction != draggedInteraction) {
             this.draggedInteraction = draggedInteraction
-            needsReset = true
+            pointerInputEventHandlerNeedsResetTick = true
         }
         if (this.sliderAdapter != sliderAdapter) {
             this.sliderAdapter = sliderAdapter
-            needsReset = true
+            pointerInputEventHandlerNeedsResetTick = true
         }
 
-        if (needsReset) {
-            pointerInputNode.resetPointerInputHandler()
+        if (pointerInputEventHandlerNeedsResetTick) {
+            pointerInputEventHandlerResetTick.value++
         }
     }
 }
