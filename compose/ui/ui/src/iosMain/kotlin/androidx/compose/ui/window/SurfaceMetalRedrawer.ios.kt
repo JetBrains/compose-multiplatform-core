5cc0a8d5e00cd2d4a49726b1bffc33270d388173
Andrei Salavei <Andrei.Salavei@jetbrains.com>
2026-03-17 16:53:58 +0100

    Fix memory spikes caused by Metal texture capturing (#2882)
    Dispose resources captured by the unused CMPDrawable.
    Prevent GCD from capturing metal texture.
    
    Fixes
    https://youtrack.jetbrains.com/issue/CMP-9939/Compose-Multiplatform-sheet-resizing-causes-excessive-IOSurface-memory-growth-on-iOS
    Fixes:
    https://youtrack.jetbrains.com/issue/CMP-9918/Clear-metal-buffers-when-ComposeUIViewController-goes-out-of-window
    
    ### Tests
    Since the issue reproduces only on the real device there is no way to
    write proper tests now.
    
    ## Release Notes
    ### Fixes - iOS
    - _(prerelease fix)_ Fix high memory consumption caused by the Compose
    container resizing when `parallelRendering` is enabled

--- compose/ui/ui/src/iosMain/kotlin/androidx/compose/ui/window/SurfaceMetalRedrawer.ios.kt
+++ compose/ui/ui/src/iosMain/kotlin/androidx/compose/ui/window/SurfaceMetalRedrawer.ios.kt
@@ -256,6 +256,16 @@ internal class SurfaceMetalRedrawer(
             }
         }
 
+    /**
+     * Closes all Skia surfaces and render targets that are currently associated with drawables.
+     */
+    fun drainSkiaSurfaces() {
+        check(NSThread.isMainThread) { "SurfaceMetalRedrawer.drainSkiaSurfaces() must be called on main thread" }
+        awaitRenderingQueueTasksCompletion()
+        metalLayer.drainDrawables()
+        disposeDrawableAssociatedResources(metalLayer.drawablesGeneration.toInt())
+    }
+
     override fun dispose() {
         check(caDisplayLink != null) { "MetalRedrawer.dispose() was called more than once" }
 
@@ -472,7 +482,7 @@ internal class SurfaceMetalRedrawer(
         val renderTarget = BackendRenderTarget.makeMetal(
             width = IOSurfaceGetWidth(drawable.surface).toInt(),
             height = IOSurfaceGetHeight(drawable.surface).toInt(),
-            texturePtr = drawable.texture.objcPtr()
+            texturePtr = drawable.drawableTexture().rawValue,
         )
 
         val surface = Surface.makeFromBackendRenderTarget(
