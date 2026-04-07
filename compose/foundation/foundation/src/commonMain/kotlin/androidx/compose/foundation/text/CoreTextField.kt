5e2b205c8fbb80ae0bb64c831d0afdf8a2c4b2c4
Oleksandr.Karpovich <a.n.karpovich@gmail.com>
2026-04-07 13:56:48 +0200

    Copy compose from 6a4264be8fc

--- compose/foundation/foundation/src/commonMain/kotlin/androidx/compose/foundation/text/CoreTextField.kt
+++ compose/foundation/foundation/src/commonMain/kotlin/androidx/compose/foundation/text/CoreTextField.kt
@@ -556,12 +556,7 @@ internal fun CoreTextField(
                     // line
                     // TextFields
                     .heightIn(min = state.minHeightForSingleLineField)
-                    .heightInLines(
-                        textStyle = textStyle,
-                        minLines = minLines,
-                        maxLines = maxLines,
-                        softWrap = softWrap,
-                    )
+                    .heightInLines(textStyle = textStyle, minLines = minLines, maxLines = maxLines)
                     .textFieldScroll(
                         scrollerPosition = scrollerPosition,
                         textFieldValue = value,
