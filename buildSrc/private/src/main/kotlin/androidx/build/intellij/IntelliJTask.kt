b486b58b85735f1e09913eada16c308035c8b073
Oleksandr.Karpovich <oleksandr.karpovich@jetbrains.com>
2026-04-08 14:40:57 +0200

    fix project gradle sync
    Change-Id: I48ed40a4f96022cfe91686b9e489358514601b45

--- buildSrc/private/src/main/kotlin/androidx/build/intellij/IntelliJTask.kt
+++ buildSrc/private/src/main/kotlin/androidx/build/intellij/IntelliJTask.kt
@@ -269,6 +269,7 @@ abstract class IntelliJTask : DefaultTask() {
                 when (ProjectLayoutType.from(this)) {
                     ProjectLayoutType.ANDROIDX -> RootIntelliJTask::class.java
                     ProjectLayoutType.PLAYGROUND -> return
+                    ProjectLayoutType.JETBRAINS_FORK -> return
                 }
             tasks.register(INTELLIJ_TASK, intellijTask)
         }
