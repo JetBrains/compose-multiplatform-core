129becbbae5eb8b51361a983bd9c641fb41fd126
Alexander Maryanovsky <msasha@gmail.com>
2026-05-20 21:05:30 +0300

    Add `MeasurableRootContent` extensions to compute intrinsic sizes where one dimension is fixed

--- compose/ui/ui/src/skikoMain/kotlin/androidx/compose/ui/layout/MeasurableRootContent.skiko.kt
+++ compose/ui/ui/src/skikoMain/kotlin/androidx/compose/ui/layout/MeasurableRootContent.skiko.kt
@@ -19,6 +19,9 @@ package androidx.compose.ui.layout
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.unit.Constraints
 import androidx.compose.ui.unit.Density
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.DpSize
+import androidx.compose.ui.unit.IntSize
 
 /**
  * The interface through which composable content can be queried for its size preferences, such as
@@ -44,3 +47,92 @@ interface MeasurableRootContent : IntrinsicMeasurable {
      */
     fun <T> measuringIn(constraints: Constraints, block: (Measured) -> T): T
 }
+
+/**
+ * Computes the intrinsic size of the content, given a fixed size on one dimension.
+ */
+private fun MeasurableRootContent.intrinsicDimension(
+    isWidth: Boolean,
+    isMax: Boolean,
+    otherDimensionSize: Dp,
+): DpSize {
+    val otherDimensionSizePx = with(density) { otherDimensionSize.roundToPx() }
+
+    val width: Int
+    val height: Int
+    if (isWidth) {
+        width = if (isMax) {
+            maxIntrinsicWidth(otherDimensionSizePx)
+        } else {
+            minIntrinsicWidth(otherDimensionSizePx)
+        }
+        height = otherDimensionSizePx
+    } else {
+        width = otherDimensionSizePx
+        height = if (isMax) {
+            maxIntrinsicHeight(otherDimensionSizePx)
+        } else {
+            minIntrinsicHeight(otherDimensionSizePx)
+        }
+    }
+
+    return with(density) {
+        DpSize(width.toDp(), height.toDp())
+    }
+}
+
+/**
+ * The returned width is the minimum intrinsic width at the given [height].
+ * The returned height is [height].
+ *
+ * @see [IntrinsicMeasurable.minIntrinsicWidth]
+ */
+@ExperimentalComposeUiApi
+fun MeasurableRootContent.minIntrinsicWidthSize(height: Dp) =
+    intrinsicDimension(
+        isWidth = true,
+        isMax = false,
+        otherDimensionSize = height,
+    )
+
+/**
+ * The returned width is the maximum intrinsic width at the given [height].
+ * The returned height is [height].
+ *
+ * @see [IntrinsicMeasurable.maxIntrinsicWidth]
+ */
+@ExperimentalComposeUiApi
+fun MeasurableRootContent.maxIntrinsicWidthSize(height: Dp) =
+    intrinsicDimension(
+        isWidth = true,
+        isMax = true,
+        otherDimensionSize = height,
+    )
+
+/**
+ * The returned height is the minimum intrinsic height at the given [width].
+ * The returned width is [width].
+ *
+ * @see [IntrinsicMeasurable.minIntrinsicHeight]
+ */
+@ExperimentalComposeUiApi
+fun MeasurableRootContent.minIntrinsicHeightSize(width: Dp) =
+    intrinsicDimension(
+        isWidth = false,
+        isMax = false,
+        otherDimensionSize = width,
+    )
+
+/**
+ * The returned width is the maximum intrinsic height at the given [width].
+ * The returned width is [width].
+ *
+ * @see [IntrinsicMeasurable.maxIntrinsicHeight]
+ */
+@ExperimentalComposeUiApi
+fun MeasurableRootContent.maxIntrinsicHeightSize(width: Dp) =
+    intrinsicDimension(
+        isWidth = false,
+        isMax = true,
+        otherDimensionSize = width,
+    )
