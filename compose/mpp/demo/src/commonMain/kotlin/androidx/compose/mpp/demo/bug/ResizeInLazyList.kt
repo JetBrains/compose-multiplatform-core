fc05c8e26cbe6f9e81b791cf7cb1a6fef9a2c51d
Shagen Ogandzhanian <shagen.ogandzhanian@jetbrains.com>
2026-05-21 10:58:22 +0200

    Back to smalle original reproduced

--- compose/mpp/demo/src/commonMain/kotlin/androidx/compose/mpp/demo/bug/ResizeInLazyList.kt
+++ compose/mpp/demo/src/commonMain/kotlin/androidx/compose/mpp/demo/bug/ResizeInLazyList.kt
@@ -17,56 +17,121 @@
 package androidx.compose.mpp.demo.bug
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.text.BasicText
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.Text
 import androidx.compose.mpp.demo.Screen
-import androidx.compose.mpp.demo.Screen.Example
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.luminance
+import androidx.compose.ui.unit.dp
 import kotlin.random.Random
 
 @OptIn(ExperimentalMaterialApi::class)
-internal val ResizeInLazyList by lazy {
-    Example("Resize In LazyList") {
-        Box(
+internal val ResizeInLazyList = Screen.Example("Resize In LazyList") {
+    val names = remember {
+        listOf(
+            "Fiona Apple",
+            "Charlie Brown",
+            "George Clooney",
+            "Penelope Cruz",
+            "Laura Dern",
+            "Nancy Drew",
+            "Rachel Green",
+            "Yara Greyjoy",
+            "Kevin Hart",
+            "Samuel Jackson",
+            "Alice Johnson",
+            "Michael Jordan",
+            "Ian McKellen",
+            "Hannah Montana",
+            "Edward Norton",
+            "Diana Prince",
+            "Julia Roberts",
+            "Bob Smith",
+            "Taylor Swift",
+            "Quentin Tarantino",
+            "Uma Thurman",
+            "Vincent Vega",
+            "Walter White",
+            "Oscar Wilde",
+            "Xavier Woods",
+        )
+    }
+
+//    MaterialTheme {
+//
+//        LazyColumn {
+//            itemsIndexed(names) { index, name ->
+//                println("$name placed as view")
+//
+//                val backgroundColor = remember(name) {
+//                    Color(
+//                        red = Random.nextFloat(),
+//                        green = Random.nextFloat(),
+//                        blue = Random.nextFloat(),
+//                        alpha = 1f,
+//                    )
+//                }
+//                val textColor = remember(backgroundColor) {
+//                    if (backgroundColor.luminance() < 0.5f) Color.White else Color.Black
+//                }
+//
+//                Text(
+//                    text = "${index + 1}. $name",
+//                    color = textColor,
+//                    modifier = Modifier
+//                        .background(backgroundColor)
+//                        .padding(16.dp)
+//                        .width(200.dp)
+//
+//                )
+//            }
+//        }
+//    }
+
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center,
+    ) {
+        LazyColumn(
             modifier = Modifier.fillMaxSize(),
-            contentAlignment = Alignment.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
         ) {
-            LazyColumn(
-                modifier = Modifier.fillMaxSize(),
-                horizontalAlignment = Alignment.CenterHorizontally,
-            ) {
-                items(1000000) {
-                    var text by remember(it) { mutableStateOf("Item $it") }
-                    val backgroundColor = remember(it) {
-                        Color(
-                            red = Random.nextFloat(),
-                            green = Random.nextFloat(),
-                            blue = Random.nextFloat(),
-                            alpha = 1f,
-                        )
-                    }
-                    BasicText(text,
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .background(backgroundColor)
-                            .clickable {
-                                text = "Clicked on item $it"
-                            }
+            itemsIndexed(names) { index, name ->
+                println("$name placed as view")
+
+                val backgroundColor = remember(name) {
+                    Color(
+                        red = Random.nextFloat(),
+                        green = Random.nextFloat(),
+                        blue = Random.nextFloat(),
+                        alpha = 1f,
                     )
                 }
-            }
-        }
+                val textColor = remember(backgroundColor) {
+                    if (backgroundColor.luminance() < 0.5f) Color.White else Color.Black
+                }
 
+                Text(
+                    text = "${index + 1}. $name",
+                    color = textColor,
+                    modifier = Modifier
+                        .background(backgroundColor)
+                        .padding(16.dp)
+                        .width(300.dp)
+
+                )
+            }
+
+        }
     }
+
 }
