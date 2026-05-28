379043e03b1ef08d662a101875bcbc0dd55694fa
ApoloApps <info@composevisualdesigner.com>
2026-05-28 14:27:19 +0200

    Revert `wrapContent` for PR review

--- compose/ui/ui/src/desktopMain/kotlin/androidx/compose/ui/awt/ComposePanel.desktop.kt
+++ compose/ui/ui/src/desktopMain/kotlin/androidx/compose/ui/awt/ComposePanel.desktop.kt
@@ -239,13 +239,17 @@ class ComposePanel @ExperimentalComposeUiApi constructor(
         // to keep the lambda describing composable content and set the content only when
         // everything is ready to avoid accidental crashes and memory leaks on all supported OS
         // types.
-        val wrappedContent: @Composable () -> Unit = {
+        val wrappedContent = wrapContent(content)
+        _composeContent = wrappedContent
+        _composeContainer?.setContent(wrappedContent)
+    }
+
+    private fun wrapContent(content: @Composable () -> Unit): @Composable () -> Unit {
+        return {
             CompositionLocalProvider(LocalAwtWindow provides windowParent) {
                 content()
             }
         }
-        _composeContent = wrappedContent
-        _composeContainer?.setContent(wrappedContent)
     }
 
     /**
