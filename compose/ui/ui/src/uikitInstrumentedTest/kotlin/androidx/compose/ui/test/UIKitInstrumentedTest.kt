080692ec9dd02ceecf051913f92e903ed5ede4d2
svastven <vendula.svastalova@jetbrains.com>
2026-06-13 22:27:30 +0200

    Increase SwipeDuration to 200 ms

--- compose/ui/ui/src/uikitInstrumentedTest/kotlin/androidx/compose/ui/test/UIKitInstrumentedTest.kt
+++ compose/ui/ui/src/uikitInstrumentedTest/kotlin/androidx/compose/ui/test/UIKitInstrumentedTest.kt
@@ -646,7 +646,7 @@ internal class UIKitInstrumentedTest(
         return dragTo(DpOffset(x ?: location.x, y ?: location.y), duration)
     }
 
-    private val SwipeDuration = 100.milliseconds
+    private val SwipeDuration = 200.milliseconds
 
     fun AccessibilityTestNode.swipe(
         fromPosition: DpRect.() -> DpOffset = { center() },
