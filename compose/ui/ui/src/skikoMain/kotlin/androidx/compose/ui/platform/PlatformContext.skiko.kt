127609e9d7bd5a6904e5b572c0f9b66864b5d64f
ApoloApps <info@composevisualdesigner.com>
2026-07-17 00:31:43 +0200

    Fix KDoc artifact due to auto-generation

--- compose/ui/ui/src/skikoMain/kotlin/androidx/compose/ui/platform/PlatformContext.skiko.kt
+++ compose/ui/ui/src/skikoMain/kotlin/androidx/compose/ui/platform/PlatformContext.skiko.kt
@@ -220,7 +220,7 @@ interface PlatformContext {
     /**
      * Executor used to perform background text measurement work.
      *
-     * If non\-null, text measurement may be offloaded to this executor to avoid
+     * If non-null, text measurement may be offloaded to this executor to avoid
      * blocking the UI thread. Default is `null` which indicates no platform
      * provided background executor.
      */
