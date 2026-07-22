8071ab8ee036a02f5c87f1e389a41577ce2a69df
Wojciech Krystyniak <wojciech.krystyniak@jetbrains.com>
2026-07-22 08:30:31 +0200

    Add sources JAR publishing to JetbrainsUnsplitPackagePlugin

--- buildSrc/plugins/src/main/kotlin/androidx/build/JetbrainsUnsplitPackagePlugin.kt
+++ buildSrc/plugins/src/main/kotlin/androidx/build/JetbrainsUnsplitPackagePlugin.kt
@@ -1,33 +1,42 @@
 package androidx.build
 
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+import javax.inject.Inject
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.artifacts.ConfigurationContainer
 import org.gradle.api.artifacts.Dependency
+import org.gradle.api.artifacts.ModuleDependency
 import org.gradle.api.artifacts.ProjectDependency
 import org.gradle.api.artifacts.dsl.DependencyHandler
-import org.gradle.api.publish.PublishingExtension
-import org.gradle.api.publish.maven.MavenPublication
-import org.gradle.kotlin.dsl.create
-import org.gradle.kotlin.dsl.project
-import org.gradle.kotlin.dsl.register
-import java.net.URI
-import javax.inject.Inject
-import org.gradle.api.artifacts.ModuleDependency
+import org.gradle.api.attributes.Category
+import org.gradle.api.attributes.DocsType
+import org.gradle.api.attributes.LibraryElements
 import org.gradle.api.attributes.Usage
 import org.gradle.api.component.AdhocComponentWithVariants
+import org.gradle.api.file.ArchiveOperations
 import org.gradle.api.model.ObjectFactory
 import org.gradle.api.provider.Provider
+import org.gradle.api.publish.PublishingExtension
+import org.gradle.api.publish.maven.MavenPublication
+import org.gradle.api.tasks.TaskProvider
+import org.gradle.api.tasks.bundling.Jar
+import org.gradle.kotlin.dsl.create
 import org.gradle.kotlin.dsl.named
+import org.gradle.kotlin.dsl.project
+import org.gradle.kotlin.dsl.register
+import org.jetbrains.kotlin.gradle.plugin.KotlinPlatformType
 
 private const val SHADOW_PLUGIN_ID = "com.gradleup.shadow"
 
-class JetbrainsUnsplitPackagePlugin : Plugin<Project> {
+class JetbrainsUnsplitPackagePlugin @Inject constructor(
+    private val archiveOperations: ArchiveOperations,
+) : Plugin<Project> {
     override fun apply(project: Project) {
         val extension = project.extensions.create<DefaultUnsplitPackageExtension>("unsplitPackage")
         configureShadowJar(project, extension)
-        configurePublishing(project, extension)
+        val sourcesJar = registerSourcesJar(project, extension)
+        configurePublishing(project, extension, sourcesJar)
     }
 
     private fun configureShadowJar(project: Project, extension: DefaultUnsplitPackageExtension) {
@@ -40,9 +49,22 @@ class JetbrainsUnsplitPackagePlugin : Plugin<Project> {
         project.tasks.named("jar") { it.enabled = false }
     }
 
-    private fun configurePublishing(
+    private fun registerSourcesJar(
         project: Project,
         extension: DefaultUnsplitPackageExtension
+    ): TaskProvider<Jar> {
+        val sourceJars =
+            extension.unsplitPackageSources.incoming.artifactView { it.isLenient = true }.files
+        return project.tasks.register<Jar>("sourcesJar") {
+            archiveClassifier.set("sources")
+            from(sourceJars.elements.map { locations -> locations.map { archiveOperations.zipTree(it) } })
+        }
+    }
+
+    private fun configurePublishing(
+        project: Project,
+        extension: DefaultUnsplitPackageExtension,
+        sourcesJar: TaskProvider<Jar>,
     ) {
         project.pluginManager.withPlugin(SHADOW_PLUGIN_ID) {
             val shadowComponent =
@@ -60,6 +82,7 @@ class JetbrainsUnsplitPackagePlugin : Plugin<Project> {
                         // publishComposeJb[ToMavenLocal] from AbstractComposePublishingTask
                         publicationContainer.register<MavenPublication>("desktop") {
                             from(project.components.findByName("shadow"))
+                            artifact(sourcesJar)
                         }
                         // Needs to be present and called "kotlinMultiplatform" to satisfy
                         // publishComposeJb[ToMavenLocal] from AbstractComposePublishingTask
@@ -112,6 +135,30 @@ internal open class DefaultUnsplitPackageExtension @Inject internal constructor(
     internal val unsplitPackage = configurationContainer.resolvable("unsplitPackage") {
         it.extendsFrom(unsplitPackageDependencies.get())
     }.get()
+
+    internal val unsplitPackageSources =
+        configurationContainer.resolvable("unsplitPackageSources") {
+            it.extendsFrom(unsplitPackageDependencies.get())
+            it.attributes { attributes ->
+                attributes.attribute(
+                    Usage.USAGE_ATTRIBUTE,
+                    objectFactory.named(Usage.JAVA_RUNTIME),
+                )
+                attributes.attribute(
+                    Category.CATEGORY_ATTRIBUTE,
+                    objectFactory.named(Category.DOCUMENTATION),
+                )
+                attributes.attribute(
+                    DocsType.DOCS_TYPE_ATTRIBUTE,
+                    objectFactory.named(DocsType.SOURCES),
+                )
+                attributes.attribute(
+                    LibraryElements.LIBRARY_ELEMENTS_ATTRIBUTE,
+                    objectFactory.named(LibraryElements.JAR),
+                )
+                attributes.attribute(KotlinPlatformType.attribute, KotlinPlatformType.jvm)
+            }
+        }.get()
 
     private val unsplitPackageTransitivesDependencies =
         configurationContainer.dependencyScope("unsplitPackageTransitivesDependencies")
