7a72deb4d8007905b854d2a0f4b2556ae65da9d4
Ivan Matkov <ivan.matkov@jetbrains.com>
2026-09-01 14:08:01 +0200

    Respect feature flags

--- compose/ui/ui/src/skikoMain/kotlin/androidx/compose/ui/platform/CompositionLocals.skiko.kt
+++ compose/ui/ui/src/skikoMain/kotlin/androidx/compose/ui/platform/CompositionLocals.skiko.kt
@@ -24,7 +24,10 @@ import androidx.compose.runtime.ProvidedValue
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.LocalSaveableStateRegistry
 import androidx.compose.runtime.staticCompositionLocalOf
+import androidx.compose.ui.ComposeUiFlags
+import androidx.compose.ui.ExperimentalMediaQueryApi
 import androidx.compose.ui.InternalComposeUiApi
+import androidx.compose.ui.LocalUiMediaScope
 import androidx.lifecycle.LifecycleOwner
 import androidx.savedstate.compose.LocalSavedStateRegistryOwner
 
@@ -65,6 +68,7 @@ val LocalPlatformPrefetchScheduler = staticCompositionLocalOf<PlatformPrefetchSc
     error("CompositionLocal LocalPlatformPrefetchScheduler not present")
 }
 
+@OptIn(ExperimentalMediaQueryApi::class)
 @Composable
 internal fun ProvidePlatformCompositionLocals(
     vararg values: ProvidedValue<*>,
@@ -87,6 +91,15 @@ internal fun ProvidePlatformCompositionLocals(
         HostDefaultProviderImpl(platformContext)
     }
 
+    val mediaScope = if (
+        !ComposeUiFlags.isMinimalistLocalsEnabled &&
+        ComposeUiFlags.isMediaQueryIntegrationEnabled
+    ) {
+        arrayOf(LocalUiMediaScope provides platformContext.mediaScope)
+    } else {
+        emptyArray()
+    }
+
     CompositionLocalProvider(
         *values,
         LocalPlatformScreenReader provides platformContext.screenReader,
@@ -96,6 +109,7 @@ internal fun ProvidePlatformCompositionLocals(
         LocalSavedStateRegistryOwner provides platformContext.architectureComponentsOwner.savedStateRegistryOwner,
         LocalSaveableStateRegistry provides saveableStateRegistry,
         LocalHostDefaultProvider provides hostDefaultProvider,
+        *mediaScope,
         content = content,
     )
 }
