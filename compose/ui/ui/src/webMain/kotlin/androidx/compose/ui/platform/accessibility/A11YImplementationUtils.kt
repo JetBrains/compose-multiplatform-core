985ca59934b5eb7023ede57f5a90afac8742d8a1
ApoloApps <info@composevisualdesigner.com>
2026-09-09 16:43:43 +0200

    Fix tests when commit changing Html node offsets from top/left to using style transforms, which does not participate in dom calculations, so overflows are not applied correctly

--- compose/ui/ui/src/webMain/kotlin/androidx/compose/ui/platform/accessibility/A11YImplementationUtils.kt
+++ compose/ui/ui/src/webMain/kotlin/androidx/compose/ui/platform/accessibility/A11YImplementationUtils.kt
@@ -30,15 +30,23 @@ import org.w3c.dom.HTMLElement
 internal fun setSizeAndPosition(
     element: HTMLElement, left: Float, top: Float, width: Float, height: Float
 ) {
+    // Note: the position must be set via left/top (not via a CSS transform).
+    // Transforms don't participate in the DOM layout: they break the layout-based geometry
+    // (offsetTop/offsetLeft), the scrollable overflow of the a11y scroll containers and
+    // the browser scroll anchoring, which ATs and browsers rely on.
     // language=javascript
     js(
         """
-       const transformValue = 'matrix(1, 0, 0, 1, ' + left + ', ' + top + ')';
+       const leftValue = "" + left + "px";
+       const topValue = "" + top + "px";
        const widthValue = "" + width + "px";
        const heightValue = "" + height + "px";
 
-       if (element.style.transform !== transformValue) {
-           element.style.transform = transformValue;
+       if (element.style.left !== leftValue) {
+           element.style.left = leftValue;
+       }
+       if (element.style.top !== topValue) {
+           element.style.top = topValue;
        }
        if (element.style.width !== widthValue) {
            element.style.width = widthValue;
