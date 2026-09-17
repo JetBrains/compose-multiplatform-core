26faca8dd742a147bb0dd73b3c122a0f1eb75297
svastven <vendula.svastalova@jetbrains.com>
2026-09-17 11:48:13 +0200

    Revert "PR review changes"
    This reverts commit f4e2b21b8ab6b6512c551829ad64f3d993c61ab7.

--- compose/ui/ui/src/skikoTest/kotlin/androidx/compose/ui/layout/DisableWindowInsetsRulersTest.kt
+++ compose/ui/ui/src/skikoTest/kotlin/androidx/compose/ui/layout/DisableWindowInsetsRulersTest.kt
@@ -28,7 +28,6 @@ import androidx.compose.ui.test.v2.runInternalSkikoComposeUiTest
 import kotlin.test.AfterTest
 import kotlin.test.Test
 import kotlin.test.assertTrue
-import kotlinx.coroutines.test.TestResult
 
 @OptIn(ExperimentalTestApi::class, InternalTestApi::class)
 class DisableWindowInsetsRulersTest {
@@ -39,37 +38,39 @@ class DisableWindowInsetsRulersTest {
     }
 
     @Test
-    fun disableWindowInsetsRulers() = runInternalSkikoComposeUiTest(
-        windowInsets = TestWindowInsets(systemBarsInsets = mutableStateOf(PlatformInsets(top = 100)))
-    ) {
+    fun disableWindowInsetsRulers() {
         WindowInsetsRulers.disable()
 
-        var left = 0f
-        var top = 0f
-        var right = 0f
-        var bottom = 0f
+        runInternalSkikoComposeUiTest(
+            windowInsets = TestWindowInsets(systemBarsInsets = mutableStateOf(PlatformInsets(top = 100)))
+        ) {
+            var left = 0f
+            var top = 0f
+            var right = 0f
+            var bottom = 0f
 
-        setContent {
-            Box(
-                Modifier.fillMaxSize().layout { measurable, constraints ->
-                    val placeable = measurable.measure(constraints)
-                    layout(placeable.width, placeable.height) {
-                        placeable.place(0, 0)
-                        left = WindowInsetsRulers.StatusBars.current.left.current(Float.NaN)
-                        top = WindowInsetsRulers.StatusBars.current.top.current(Float.NaN)
-                        right = WindowInsetsRulers.StatusBars.current.right.current(Float.NaN)
-                        bottom = WindowInsetsRulers.StatusBars.current.bottom.current(Float.NaN)
+            setContent {
+                Box(
+                    Modifier.fillMaxSize().layout { measurable, constraints ->
+                        val placeable = measurable.measure(constraints)
+                        layout(placeable.width, placeable.height) {
+                            placeable.place(0, 0)
+                            left = WindowInsetsRulers.StatusBars.current.left.current(Float.NaN)
+                            top = WindowInsetsRulers.StatusBars.current.top.current(Float.NaN)
+                            right = WindowInsetsRulers.StatusBars.current.right.current(Float.NaN)
+                            bottom = WindowInsetsRulers.StatusBars.current.bottom.current(Float.NaN)
+                        }
                     }
-                }
-            )
+                )
+            }
+
+            waitForIdle()
+
+            assertTrue(left.isNaN())
+            assertTrue(top.isNaN())
+            assertTrue(right.isNaN())
+            assertTrue(bottom.isNaN())
         }
-
-        waitForIdle()
-
-        assertTrue(left.isNaN())
-        assertTrue(top.isNaN())
-        assertTrue(right.isNaN())
-        assertTrue(bottom.isNaN())
     }
 }
 
