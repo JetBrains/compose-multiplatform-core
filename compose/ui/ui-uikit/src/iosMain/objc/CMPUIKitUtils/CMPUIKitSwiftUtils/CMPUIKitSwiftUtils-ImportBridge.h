5bb3877403e6b5f3aa23aead85154a8d4fc7bc82
svastven <vendula.svastalova@jetbrains.com>
2026-09-22 17:33:05 +0200

    Bridge UIKit imports for Swift cinterop

---
+++ compose/ui/ui-uikit/src/iosMain/objc/CMPUIKitUtils/CMPUIKitSwiftUtils/CMPUIKitSwiftUtils-ImportBridge.h
@@ -0,0 +1,21 @@
+/*
+ * Copyright 2026 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// The generated Swift Objective-C header forward-declares UIKit classes such as UIView.
+// Import UIKit first so Kotlin/Native resolves them as platform.UIKit classes rather than
+// creating separate forward-declared types.
+#import <UIKit/UIKit.h>
+#import "CMPUIKitSwiftUtils-Swift.h"
