8ef34bc15b4d62e67398409f0beca7b4e15f76fb
svastven <vendula.svastalova@jetbrains.com>
2026-09-22 17:30:49 +0200

    Document Swift compatibility linking workaround

--- buildSrc-fork/public/src/main/kotlin/org/jetbrains/androidx/build/SwiftCompatibilityLinking.kt
+++ buildSrc-fork/public/src/main/kotlin/org/jetbrains/androidx/build/SwiftCompatibilityLinking.kt
@@ -26,6 +26,13 @@ import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget
 import org.jetbrains.kotlin.gradle.plugin.mpp.NativeBinary
 import org.jetbrains.kotlin.konan.target.KonanTarget
 
+/**
+ * Adds the active Xcode toolchain's Swift library directory to iOS native links.
+ *
+ * Kotlin/Native does not currently add this directory itself (KT-69793). Resolving it at final link time
+ * avoids publishing an absolute Xcode path in klib metadata; Swift auto-link metadata selects the needed
+ * runtime and compatibility libraries.
+ */
 fun Project.configureSwiftCompatibilityLinking() {
     plugins.withId("org.jetbrains.kotlin.multiplatform") {
         extensions
