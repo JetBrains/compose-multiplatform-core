306520ca9ac8e2332d596efe50af410afa18e833
svastven <vendula.svastalova@jetbrains.com>
2026-09-24 02:17:17 +0200

    Remove unrelated ComposeSceneMediator formatting

--- compose/ui/ui/src/iosMain/kotlin/androidx/compose/ui/scene/ComposeSceneMediator.ios.kt
+++ compose/ui/ui/src/iosMain/kotlin/androidx/compose/ui/scene/ComposeSceneMediator.ios.kt
@@ -431,7 +431,7 @@ internal class ComposeSceneMediator(
 
                 down || up
             },
-            onScreenReaderActive = { platformScreenReader.isActive = it },
+            onScreenReaderActive = { platformScreenReader.isActive = it }
         )
     }
 
@@ -804,6 +804,7 @@ internal class ComposeSceneMediator(
         frameChoreographer.removeListener(frameChoreographerListener)
         prefetchScheduler.dispose()
         activitiesHandler.dispose()
+
         _overlayView.dispose()
         keyboardManager.dispose()
         _backgroundView.dispose()
