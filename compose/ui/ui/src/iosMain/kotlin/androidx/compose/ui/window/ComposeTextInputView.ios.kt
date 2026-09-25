2217725fb20157787f9cab1cb298e976beb5bcd7
Andrei Salavei <Andrei.Salavei@jetbrains.com>
2026-09-25 13:51:15 +0200

    Update compose/ui/ui/src/iosMain/kotlin/androidx/compose/ui/window/ComposeTextInputView.ios.kt
    Co-authored-by: Vendula Švastalová <vendula.svastalova@jetbrains.com>

--- compose/ui/ui/src/iosMain/kotlin/androidx/compose/ui/window/ComposeTextInputView.ios.kt
+++ compose/ui/ui/src/iosMain/kotlin/androidx/compose/ui/window/ComposeTextInputView.ios.kt
@@ -87,7 +87,7 @@ internal class ComposeTextInputView(
                 }
                 field = value
                 if (isFirstResponder) {
-                    value?.onFocus()
+                    field?.onFocus()
                 }
                 if (value == null) {
                     hideTextMenu()
