2c990232f1d052e03dad2d9b9902fdafd4e44240
Oleksandr Karpovich <a.n.karpovich@gmail.com>
2026-09-25 10:41:58 +0200

    Ensure backing input focus requests do not override pending ones on web (#3446)
    Starting from iOS Safari 27, repeated focus requests override previous
    pending ones. If the new request happens not during user interaction
    (e.g. in rAF), all pending focus requests are ignored.
    
    Fixes https://youtrack.jetbrains.com/issue/CMP-10825
    
    ## Testing
    This should be tested by QA
    
    ## Release Notes
    ### Fixes - Web
    - Adapt backing text input focus behaviour for iOS Safari 27.0

--- compose/ui/ui/src/webMain/kotlin/androidx/compose/ui/platform/BackingDomInput.kt
+++ compose/ui/ui/src/webMain/kotlin/androidx/compose/ui/platform/BackingDomInput.kt
@@ -62,14 +62,21 @@ internal class BackingDomInput(
     }
 
     fun focus() {
-        // we focus twice to be sure that ios and non-ios browser both manage to focus
+        // we request focus twice to be sure that ios and non-ios browser both manage to focus
         // see https://youtrack.jetbrains.com/issue/CMP-8013
         // and https://youtrack.jetbrains.com/issue/CMP-7836/
-        backingElement.focus()
-        window.requestAnimationFrame {
-            backingElement.focus()
-        }
+        // Safari will ignore a focus request if it's called not during user interaction.
 
+        if (!inputStrategy.isInputActive()) backingElement.focus()
+        window.requestAnimationFrame {
+            // TODO: requesting focus in rAF is rather redundant; need to verify
+            // A new focus request will replace the previous one in iOS Safari 27+.
+            // A new request made in rAF will have userIsInteracting=false.
+            // Such a request will be ignored and the software keyboard won't show up.
+            // See https://github.com/WebKit/WebKit/commit/64ee64bad41e5d511964c29b8ab00ee77269821a
+            // Therefore, make a new focus request conditionally:
+            if (!inputStrategy.isInputActive()) backingElement.focus()
+        }
     }
 
     fun blur() {
